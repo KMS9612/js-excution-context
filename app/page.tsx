00520a0c22459fb8c053c5ac14fc7aadd9f3ce8c
KMS9612 <kimdev9612@gmail.com>
2024-05-04 17:17:58 +0900

    JS-Excution-Context first commit (Next.js14, Emotion, Material-UI, Monaco-Editor, TypeScript...)

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,62 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import { Box, Typography } from "@mui/material";
+import Editor from "@monaco-editor/react";
+import * as parser from "@babel/parser";
 
 export default function Home() {
+  const onChangeEditorValue = (value: string | undefined) => {
+    if (value !== undefined) {
+      // Babel parser를 사용하여 AST 생성
+      const ast = parser.parse(value, {
+        // 파싱할 때 사용할 옵션들
+        sourceType: "module", // "script" 또는 "module" 중 선택
+        plugins: [
+          // 사용 중인 JavaScript 언어 기능 또는 실험적 기능에 맞는 플러그인을 추가
+          "jsx", // JSX 문법 사용 시
+          "typescript", // TypeScript 사용 시
+        ],
+      });
+
+      console.log(ast);
+    }
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box>
+      <Typography variant="h2" color={"#fff"}>
+        JavaScript Excution Context
+      </Typography>
+      <Box
+        style={{
+          width: "100%",
+          display: "flex",
+          padding: "0 15px",
+          gap: "10px",
+        }}
+      >
+        {/* Editor */}
+        <Box
+          style={{
+            width: "50%",
+            height: "70vh",
+            borderRadius: "15px",
+            overflow: "hidden",
+          }}
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          <Editor
+            width={"100%"}
+            height="100%"
+            defaultLanguage="javascript"
+            defaultValue="// some comment"
+            theme="vs-dark"
+            onChange={onChangeEditorValue}
+          ></Editor>
+        </Box>
+        {/* Result */}
+        <Box style={{ width: "50%", border: "1px solid #fff" }}>
+          Result Here
+        </Box>
+      </Box>
+    </Box>
   );
 }
